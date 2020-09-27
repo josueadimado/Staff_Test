8799eec71f45de3ffeb3c0f874ce46fab744c7ce
Theophilus Nutifafa <theophylusnhutiphapha@gmail.com>
2020-09-27 04:06:39 -0700

    Update settings.py

--- Staff_Test/settings.py
+++ Staff_Test/settings.py
@@ -76,10 +76,22 @@ WSGI_APPLICATION = 'Staff_Test.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'staff',
+        'USER':'root',
+        'HOST':'127.0.0.1',
+        'PASSWORD':'hello222',
+        'PORT': '5432',
+        'OPTIONS': {'charset': 'utf8mb4'},
     }
 }
 
