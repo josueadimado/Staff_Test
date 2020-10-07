8bc7224e4779c141eef592aacb559f860dab0536
unknown <quojo@moonsquare.co>
2020-10-07 16:25:53 -0700

    user admin helps, it does

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from .models import *
+from django.contrib.auth.admin import UserAdmin
 
 
-class CustomUserAdmin():
+class CustomUserAdmin(UserAdmin):
     pass
 
 # Register your models here.
