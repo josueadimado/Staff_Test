131732497eb28f285962ea9b64445d03218ab1ed
unknown <quojo@moonsquare.co>
2020-10-07 16:24:09 -0700

    let's toast a little, its fun

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import *
 
+
+class CustomUserAdmin():
+    pass
+
 # Register your models here.
-admin.site.register(CustomUser)
+admin.site.register(CustomUserAdmin,CustomUser)
 
