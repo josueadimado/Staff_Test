06de5932c1834934b42c2404bdc6f59179f93126
Theophilus Nutifafa <theophylusnhutiphapha@gmail.com>
2020-11-24 17:46:46 +0000

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -4,4 +4,4 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class CustomUser(AbstractUser):
-    pass
+    recover_token = models.CharField(max_length=60,null=True)
