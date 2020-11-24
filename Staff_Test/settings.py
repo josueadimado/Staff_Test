9d031fc89926e8b17a0600bd1bb17dd8ad0ee143
Theophilus Nutifafa <theophylusnhutiphapha@gmail.com>
2020-11-24 21:06:56 +0000

    Update settings.py

--- Staff_Test/settings.py
+++ Staff_Test/settings.py
@@ -139,3 +139,13 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'pythonwithellie@gmail.com'
+EMAIL_HOST_PASSWORD = 'justdonttryit22#@'
+
