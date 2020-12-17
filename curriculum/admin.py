15e4ef70b7a12bb8ce4907d50eccfc84410f4613
Theophilus Nutifafa <theophylusnhutiphapha@gmail.com>
2020-12-17 10:47:33 +0000

    Update admin.py

--- curriculum/admin.py
+++ curriculum/admin.py
@@ -29,9 +29,20 @@ class ResultAdmin(admin.ModelAdmin):
     model = Result
     inlines = [ResultSectionInline,]
 
+class ReviewSectionInline(admin.TabularInline):
+    model = ReviewAnswer
+#     readonly_fields = ['name','mean','sd']
+    extra = 0
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    model = Review
+    inlines = [ReviewSectionInline,]
+
 
 # Register your models here.
 admin.site.register(Section)
 admin.site.register(Result,ResultAdmin)
+admin.site.register(Review,ReviewAdmin)
 admin.site.register(Indicator,IndicatorAdmin)
 admin.site.register(IndicatorStatement,StatementAdmin)
